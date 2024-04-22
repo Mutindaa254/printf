9eb3c40fb8c607c7af5c9d8a5617f622aed8d7e1
Moses Mumo <mosesm5499@gmail.com>
2024-04-22 15:35:47 +0300

    print_unsigned.c

--- test_files/print_unsigned.c
+++ test_files/print_unsigned.c
@@ -2,7 +2,6 @@
 
 int print_unsigned(unsigned int value){
    int count = 0;
-  
     /* First remove the last digit of number and print 
     the remaining digits using recursion, then print
     the last digit
